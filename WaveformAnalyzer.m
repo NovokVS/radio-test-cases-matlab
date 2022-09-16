63e5820522bb3cdb748b22fd7855e35916946726
Galaxy86-code <mukukinov@gmail.com>
2022-09-16 16:42:40 +0300

    Добавлено описание в класс WaveformAnalyzer

--- WaveformAnalyzer.m
+++ WaveformAnalyzer.m
@@ -1,23 +1,63 @@
-classdef WaveformAnalyzer
-    %WAVEFORMANALYZER Summary of this class goes here
-    %   Detailed explanation goes here
-    
+classdef WaveformAnalyzer < handle
+    %% Описание класса
+    %
+    % 1. Класс читает данные (во временной области) на выходе OFDM модулятора сигнала, а также информацию о параметрах формирователя
+    %
+    % 2. Строит метрики: спектральная плотность мощности в частотной области, графическое представление созвездия на комплексной плоскости,
+    % среднеквадратичное значение модуля вектора ошибки (EVM)
+    %
+    % Входные данные:
+    %
+    % waveformSource - массив содержащий отчеты baseband сигнала во временной области на выходе OFDM модулятора
+    %
+    % waveformInfo - структура с параметрами OFDM модулятора и пейлоуда:
+    %       Nfft               - кол-во спектрально-временных отчетов дискретного преобразования Фурье
+    %       SampleRate         - частота семплирования [Гц]
+    %       CyclicPrefixLengths/SymbolLengths - длины циклического преффикса и OFDM символов [кол-во временных отчетов]
+    %       SymbolsCount       - кол-во символов на слот радиокадра
+    %       subCarriersCount   - кол-во поднесущих
+    %       payloadSymbols     - информационные символы
+    %       payloadSymbolsIdxs - индексы ресурсных элементов отведенные для передачи payloadSymbols
+    %
+    % Поля класса:
+    %
+    %       rmsEvm            - среднеквадратичное значение модуля вектора ошибки
+    %       waveformMeanPower - среднеквадратичное значение мощности сигнала
+    %       channelBandwidth  - ширина полосы канала
+    %       noiseMeanPower    - среднеквадратичное значение мощности шума
+    %       modulationType    - тип модуляционной схемы
+    %       waveformDuration  - длина анализируемого сигнала
+    %
+
     properties
-        Property1
+        rmsEvm
+        waveformMeanPower
+        channelBandwidth
+        noiseMeanPower
+        modulationType
+        waveformDuration
     end
-    
+
     methods
-        function obj = WaveformAnalyzer(inputArg1,inputArg2)
-            %WAVEFORMANALYZER Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function this = WaveformAnalyzer()
+            % Конструктор класса. Чтение waveform-ы во временной области и структуры с информацией
+            % необходимой для дальнейшей обработки данных и заполнения полей класса
         end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+
+        function calcWaveformParameters(this)
+
+        end
+
+        function plotPowerSpectrumDensity(this)
+
+        end
+
+        function plotPayloadConstellation(this)
+
+        end
+
+        function calcEvmPerformance(this)
+
         end
     end
 end
-
